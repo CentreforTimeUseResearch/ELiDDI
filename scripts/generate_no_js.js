c2227a4274e30e07f8778afde16a5c05ec2b6936
Jamie Smith <zz.james@gmail.com>
2026-05-19 13:25:59 +0100

    feat: test parse json activties data during vite build

--- scripts/generate_no_js.js
+++ scripts/generate_no_js.js
@@ -1,1 +1,27 @@
-console.log('<span>hello world</span>');
+import fs from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+const filePath = path.join(__dirname, '../config/activities.json');
+
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  processJson(data);
+});
+
+function processJson(jsonData) {
+    const activities = JSON.parse(jsonData);
+    activities.timeline.forEach(
+        (timeline) => {
+            console.log(timeline.name)
+        }
+    )
+}
+
+//console.log('<span>'+filePath+'</span>');
